07e89d19206f703672f3b7871b5c6dcbc65bfab0
jsrr806 <rjdhiraj806@gmail.com>
2018-06-23 16:16:36 +0530

    Add existing file

--- search/views.py
+++ search/views.py
@@ -15,7 +15,7 @@ class SearchProductView(ListView):
 
     def get_queryset(self, *args, **kwargs):
         query = self.request.GET.get('q', None) # method_dict['q']
-        print(query)
+        print(query+'s')
         if query is not None:
             return Product.objects.filter(Q(title__icontains=query)|Q(company__icontains=query)|Q(model__icontains=query))
 
